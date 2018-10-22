85cf1d263a26772fa1dfd999b0eca6f7f8851359
Jake Dallimore <jake@moodle.com>
2018-10-22 17:26:27 +0800

    MDL-63549 core_message: send_fake_message_to_convesation test method

--- message/tests/messagelib_test.php
+++ message/tests/messagelib_test.php
@@ -109,6 +109,33 @@ class core_message_messagelib_testcase extends advanced_testcase {
     }
 
     /**
+     * @param stdClass $userfrom user object of the one sending the message.
+     * @param int $convid id of the conversation in which we'll send the message.
+     * @param string $message message to send.
+     * @param int $time the time the message was sent.
+     * @return int the id of the message which was sent.
+     * @throws dml_exception if the conversation doesn't exist.
+     */
+    protected function send_fake_message_to_conversation(\stdClass $userfrom, int $convid, string $message = 'Hello world!',
+            int $time = null) : int {
+        global $DB;
+
+        $conversationrec = $DB->get_record('message_conversations', ['id' => $convid], 'id', MUST_EXIST);
+        $conversationid = $conversationrec->id;
+
+        $time = $time ?? time();
+        $record = new stdClass();
+        $record->useridfrom = $userfrom->id;
+        $record->conversationid = $conversationid;
+        $record->subject = 'No subject';
+        $record->fullmessage = $message;
+        $record->smallmessage = $message;
+        $record->timecreated = $time;
+
+        return $DB->insert_record('messages', $record);
+    }
+
+    /**
      * Test message_get_blocked_users.
      */
     public function test_message_get_blocked_users() {
