3af85fee71ef29288fcd183927cf9596fd26c53b
Amaia Anabitarte <amaia@moodle.com>
2018-10-24 18:23:40 +0200

    MDL-63282 core_message: Search API
    Fixing feedback from 1st PR by Sara Arjona

--- message/tests/api_test.php
+++ message/tests/api_test.php
@@ -286,60 +286,77 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         $user1->firstname = 'User search';
         $user1->lastname = 'One';
         $user1 = self::getDataGenerator()->create_user($user1);
+
         // Set as the user performing the search.
         $this->setUser($user1);
+
         $user2 = new stdClass();
         $user2->firstname = 'User search';
         $user2->lastname = 'Two';
         $user2 = self::getDataGenerator()->create_user($user2);
+
         $user3 = new stdClass();
         $user3->firstname = 'User search';
         $user3->lastname = 'Three';
         $user3 = self::getDataGenerator()->create_user($user3);
+
         $user4 = new stdClass();
         $user4->firstname = 'User';
         $user4->lastname = 'Four';
         $user4 = self::getDataGenerator()->create_user($user4);
+
         $user5 = new stdClass();
         $user5->firstname = 'User search';
         $user5->lastname = 'Five';
         $user5 = self::getDataGenerator()->create_user($user5);
+
         $user6 = new stdClass();
         $user6->firstname = 'User search';
         $user6->lastname = 'Six';
         $user6 = self::getDataGenerator()->create_user($user6);
+
         $user7 = new stdClass();
         $user7->firstname = 'User search';
         $user7->lastname = 'Seven';
         $user7 = self::getDataGenerator()->create_user($user7);
+
         // Add some users as contacts.
         \core_message\api::add_contact($user1->id, $user2->id);
         \core_message\api::add_contact($user1->id, $user3->id);
         \core_message\api::add_contact($user1->id, $user4->id);
+
         $time = 1;
+
         // Send a message to a non contact.
         $this->send_fake_message($user1, $user6, 'Don\'t block me.', 0, $time);
+
         // Receive a message from a non contact.
         $this->send_fake_message($user7, $user1, 'Don\'t block me.', 0, $time);
+
         // Perform a search $CFG->messagingallusers setting enabled.
         set_config('messagingallusers', 1);
         list($contacts, $noncontacts) = \core_message\api::message_search_users($user1->id, 'search');
+
         // Check that we retrieved the correct contacts.
         $this->assertCount(2, $contacts);
         $this->assertEquals($user3->id, $contacts[0]->userid);
         $this->assertEquals($user2->id, $contacts[1]->userid);
+
         // Check that we retrieved the correct non-contacts.
         $this->assertCount(3, $noncontacts);
         $this->assertEquals($user5->id, $noncontacts[0]->userid);
         $this->assertEquals($user7->id, $noncontacts[1]->userid);
         $this->assertEquals($user6->id, $noncontacts[2]->userid);
+
         // Perform a search $CFG->messagingallusers setting disabled.
         set_config('messagingallusers', 0);
         list($contacts, $noncontacts) = \core_message\api::message_search_users($user1->id, 'search');
+
         // Check that we retrieved the correct contacts.
         $this->assertCount(2, $contacts);
         $this->assertEquals($user3->id, $contacts[0]->userid);
         $this->assertEquals($user2->id, $contacts[1]->userid);
+
         // Check that we retrieved the correct non-contacts.
         $this->assertCount(2, $noncontacts);
         $this->assertEquals($user7->id, $noncontacts[0]->userid);
@@ -380,10 +397,13 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         \core_message\api::add_contact($user1->id, $user2->id);
 
         $time = 1;
+
         // Send a message to a non contact.
         $this->send_fake_message($user1, $user2, 'Don\'t block me.', 0, $time);
+
         // Receive a message from a non contact.
         $this->send_fake_message($user3, $user1, 'Don\'t block me.', 0, $time);
+
         // Create a group conversation with users.
         \core_message\api::create_conversation(\core_message\api::MESSAGE_CONVERSATION_TYPE_GROUP,
             array($user1->id, $user2->id, $user3->id, $user4->id),
@@ -444,46 +464,61 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         $user1->firstname = 'User search';
         $user1->lastname = 'One';
         $user1 = self::getDataGenerator()->create_user($user1);
+
         // Set as the user performing the search.
         $this->setUser($user1);
+
         $user2 = new stdClass();
         $user2->firstname = 'User search';
         $user2->lastname = 'Two';
         $user2 = self::getDataGenerator()->create_user($user2);
+
         $user3 = new stdClass();
         $user3->firstname = 'User search';
         $user3->lastname = 'Three';
         $user3 = self::getDataGenerator()->create_user($user3);
+
         $user4 = new stdClass();
         $user4->firstname = 'User';
         $user4->lastname = 'Four';
         $user4 = self::getDataGenerator()->create_user($user4);
+
         $user5 = new stdClass();
         $user5->firstname = 'User search';
         $user5->lastname = 'Five';
         $user5 = self::getDataGenerator()->create_user($user5);
+
         // Add a user as contact.
         \core_message\api::add_contact($user1->id, $user2->id);
+
         $time = 1;
+
         // Send a messages to a non contact.
         $this->send_fake_message($user1, $user2, 'Don\'t block me.', 0, $time);
+
         // Receive a messages from a non contact.
         $this->send_fake_message($user3, $user1, 'Don\'t block me.', 0, $time);
+
         // Create a group conversation with users.
         \core_message\api::create_conversation(\core_message\api::MESSAGE_CONVERSATION_TYPE_GROUP,
             array($user1->id, $user2->id, $user4->id),
             'Project chat');
+
         // Perform a search $CFG->messagingallusers setting enabled.
         set_config('messagingallusers', 1);
         list($contacts, $noncontacts) = \core_message\api::message_search_users($user1->id, 'search');
+
         // Check that we retrieved the correct contacts.
         $this->assertCount(1, $contacts);
+
         // Check that we retrieved the correct conversations for contacts.
         $this->assertCount(2, $contacts[0]->conversations);
+
         // Check that we retrieved the correct non-contacts.
         $this->assertCount(2, $noncontacts);
         $this->assertEquals($user5->id, $noncontacts[0]->userid);
         $this->assertEquals($user3->id, $noncontacts[1]->userid);
+
         // Check that we retrieved the correct conversations for non-contacts
         $this->assertCount(0, $noncontacts[0]->conversations);
         $this->assertCount(1, $noncontacts[1]->conversations);
@@ -498,57 +533,73 @@ class core_message_api_testcase extends core_message_messagelib_testcase {
         $user1->firstname = 'User search';
         $user1->lastname = 'One';
         $user1 = self::getDataGenerator()->create_user($user1);
+
         // Set as the user performing the search.
         $this->setUser($user1);
+
         $user2 = new stdClass();
         $user2->firstname = 'User search';
         $user2->lastname = 'Two';
         $user2 = self::getDataGenerator()->create_user($user2);
+
         $user3 = new stdClass();
         $user3->firstname = 'User search';
         $user3->lastname = 'Three';
         $user3 = self::getDataGenerator()->create_user($user3);
+
         $user4 = new stdClass();
         $user4->firstname = 'User';
         $user4->lastname = 'Four';
         $user4 = self::getDataGenerator()->create_user($user4);
+
         $user5 = new stdClass();
         $user5->firstname = 'User search';
         $user5->lastname = 'Five';
         $user5 = self::getDataGenerator()->create_user($user5);
+
         // Add some contact_requests.
         \core_message\api::create_contact_request($user1->id, $user2->id);
         \core_message\api::create_contact_request($user3->id, $user1->id);
+
         // Perform a search $CFG->messagingallusers setting enabled.
         set_config('messagingallusers', 1);
         list($contacts, $noncontacts) = \core_message\api::message_search_users($user1->id, 'search');
+
         // Check that we retrieved the correct contacts.
         $this->assertCount(0, $contacts);
+
         // Check that we retrieved the correct non-contacts.
         $this->assertCount(3, $noncontacts);
         $this->assertEquals($user5->id, $noncontacts[0]->userid);
         $this->assertEquals($user3->id, $noncontacts[1]->userid);
         $this->assertEquals($user2->id, $noncontacts[2]->userid);
+
         // Check that we retrieved the correct conversations for non-contacts
         $this->assertCount(0, $noncontacts[0]->contactrequests);
         $this->assertCount(1, $noncontacts[1]->contactrequests);
         $this->assertCount(1, $noncontacts[2]->contactrequests);
+
         // Perform same search after confirming or declining contact requests.
         // Confirm a contact request.
         \core_message\api::confirm_contact_request($user1->id, $user2->id);
+
         // Decline a contact request.
         \core_message\api::decline_contact_request($user3->id, $user1->id);
+
         // Perform a search $CFG->messagingallusers setting enabled.
-        set_config('messagingallusers', 1);
         list($contacts, $noncontacts) = \core_message\api::message_search_users($user1->id, 'search');
+
         // Check that we retrieved the correct contacts.
         $this->assertCount(1, $contacts);
+
         // Check that we retrieved the correct contact requests for contacts.
         $this->assertCount(0, $contacts[0]->contactrequests);
+
         // Check that we retrieved the correct non-contacts.
         $this->assertCount(2, $noncontacts);
         $this->assertEquals($user5->id, $noncontacts[0]->userid);
         $this->assertEquals($user3->id, $noncontacts[1]->userid);
+
         // Check that we retrieved the correct contact requests for non-contacts
         $this->assertCount(0, $noncontacts[0]->contactrequests);
         $this->assertCount(0, $noncontacts[1]->contactrequests);
