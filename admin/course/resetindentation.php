8170f4f1d19cd27c6fc5fc6a940c6e245d2554f2
Amaia Anabitarte <amaia@moodle.com>
2023-02-16 11:19:52 +0100

    a

---
+++ admin/course/resetindentation.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Reset course indentation
+ *
+ * @copyright 2023 Amaia Anabitarte <amaia@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../config.php');
+$PAGE->set_url('/admin/course/resetindentation.php');
+$PAGE->set_context(context_system::instance());
+
+require_admin();
+
+$strtitle = get_string('resetindentation', 'admin');
+
+$PAGE->set_title($strtitle);
+$PAGE->set_heading($strtitle);
+
+echo $OUTPUT->header();
+echo $OUTPUT->confirm(get_string(get_string('resetcourseindentation', 'admin'));
+echo $OUTPUT->footer();
