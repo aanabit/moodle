b980cbbea20d2f37ca563f653ab8f8a8de27d773
Amaia Anabitarte <amaia@moodle.com>
2025-07-30 17:45:36 +0200

    MDL-85853 mod_hp5activity: Implement group based information in overview

--- public/mod/h5pactivity/classes/courseformat/overview.php
+++ public/mod/h5pactivity/classes/courseformat/overview.php
@@ -122,8 +122,16 @@ class overview extends \core_courseformat\activityoverviewbase {
             return null;
         }
 
-        $attempts = $this->manager->count_users_attempts();
-        $participants = get_users_by_capability($this->context, 'mod/h5pactivity:submit');
+        $groups = [];
+        if ($this->needs_filtering_by_groups()) {
+            $groups = $this->get_groups_for_filtering();
+        }
+        $attempts = $this->manager->count_users_attempts($groups);
+        $participants = get_users_by_capability(
+            context: $this->context,
+            capability: 'mod/h5pactivity:submit',
+            groups: array_keys($groups),
+        );
         $params = [
             'count' => count($attempts),
             'total' => count($participants),
@@ -147,8 +155,12 @@ class overview extends \core_courseformat\activityoverviewbase {
             return null;
         }
 
-        $totalattempts = $this->manager->count_attempts();
-        $totalusers = $this->manager->count_users_attempts();
+        $groups = [];
+        if ($this->needs_filtering_by_groups()) {
+            $groups = $this->get_groups_for_filtering();
+        }
+        $totalattempts = $this->manager->count_attempts(groups: $groups);
+        $totalusers = $this->manager->count_users_attempts(groups: $groups);
 
         $averageattempts = 0;
         if ($totalusers && count($totalusers) > 0 && $totalattempts) {
